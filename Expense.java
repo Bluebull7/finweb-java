9b5706a68c76713feedfe4939f7aaa6c33ae11c5
Lance H <lsaucyfx@gmail.com>
2024-02-12 11:59:21 -0500

    added expense class

---
+++ Expense.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Expense {
+    
+    private int expenseId;
+    private Date date;
+    private double amount;
+    private TransactionCategory category;
+
+    public String getExpenseDetails() {
+    // implement me
+
+    }
+}
+
+
